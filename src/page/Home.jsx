5595abff1199b55bc365fa8e1bf65101dd83850e
Abhishek <anand2000ys@gmail.com>
2023-06-08 15:03:32 +0530

    homepage is ready

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,40 @@
+import { Email, GitHub, LinkedIn } from "@mui/icons-material";
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Abhishek</h2>
+        <div className="prompt">
+          <p>
+            I am a dedicated and enthusiastic third-year undergraduate student
+            with a passion for learning and creating.
+          </p>
+          <LinkedIn />
+          <Email />
+          <GitHub />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              React.js, Vue.js, HTML, CSS, React Native, NPM, Bootstrap,
+              MaterialUI
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, TypeScript, Java, C++, C, Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
