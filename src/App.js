83e697fc44fdfa5b9ee57cae1ee847185e8c1f1f
Abhishek <anand2000ys@gmail.com>
2023-06-08 10:16:56 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/HomeSection/Home";
+import Projects from "./Components/ProjectsSection/Projects";
+import Experience from "./Components/ExperienceSection/Experience";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
