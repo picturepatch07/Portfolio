bde7129ebfa560b401c61dffb1681811848490a3
Abhishek <anand2000ys@gmail.com>
2023-06-09 08:46:52 +0530

    demo projects added

--- src/page/Projects.jsx
+++ src/page/Projects.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import ProjectItem from "../Components/ProjectSection/ProjectItem";
+import { ProjectList } from "../helper/ProjectList";
+import "./Projects.css";
 
 function Project() {
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
-      <div className="projectList"></div>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem
+              key={idx}
+              image={project.image}
+              name={project.name}
+              link={project.link}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
