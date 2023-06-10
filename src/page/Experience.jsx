ac6825d39e3835aaf597a879808b6695bd777eec
Abhishek <anand2000ys@gmail.com>
2023-06-10 20:01:03 +0530

    experience page added

--- src/page/Experience.jsx
+++ src/page/Experience.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { School } from "@mui/icons-material";
+import "./Experience.css";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2017 - 2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<School />}
+        >
+          <h3 className="vertical-timeline-element-title">Class XII</h3>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
